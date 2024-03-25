eb7ce1b36cc432196d1805b458f101c0d5198f4f
SUJEETH-K <sujeeth4702@gmail.com>
2024-03-25 10:56:05 +0530

    changes

--- HTML/SELF_EXPLORATION/sample.js
+++ HTML/SELF_EXPLORATION/sample.js
@@ -1,12 +1,58 @@
+// class Employee{
+//     empId: number;
+//     empName: string;
+//     constructor(){
+//         this.empId = 1;
+//         this.empName = "sujeeth";
+//     }
+//     display(){
+//         console.log(this.empId + " " + this.empName);
+//     }
+// }
+// let Employee1 = new Employee();
+// Employee1.display();
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//var sam = ["ana", 3, "one", 3.23, true];
 var Employee = /** @class */ (function () {
-    function Employee() {
-        this.empId = 1;
-        this.empName = "sujeeth";
+    function Employee(empId, empName) {
+        this.empId = empId;
+        this.empName = empName;
     }
-    Employee.prototype.display = function () {
+    Employee.prototype.show = function () {
         console.log(this.empId + " " + this.empName);
     };
     return Employee;
 }());
-var Employee1 = new Employee();
-Employee1.display();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(empid, empname) {
+        return _super.call(this, empid, empname) || this;
+    }
+    return Manager;
+}(Employee));
+var man1 = new Manager(2, "kavin");
+man1.show();
+// let set = new Set();
+// set.add("2");
+// set.add(true);
+// set.add("kavin");
+// set.add("kavin");
+// let map1 = new Map();
+// map1.set("ind", "India");
+// map1.set("in", "indonesia");
+//map1.get();
+// console.log(map1.keys());
--- HTML/SELF_EXPLORATION/sample.ts
+++ HTML/SELF_EXPLORATION/sample.ts
@@ -1,13 +1,52 @@
+// class Employee{
+//     empId: number;
+//     empName: string;
+//     constructor(){
+//         this.empId = 1;
+//         this.empName = "sujeeth";
+//     }
+//     display(){
+//         console.log(this.empId + " " + this.empName);
+//     }
+// }
+// let Employee1 = new Employee();
+// Employee1.display();
+
+//var sam = ["ana", 3, "one", 3.23, true];
+
+
 class Employee{
     empId: number;
     empName: string;
-    constructor(){
-        this.empId = 1;
-        this.empName = "sujeeth";
+    constructor(empId: number, empName: string){
+        this.empId = empId;
+        this.empName = empName;
     }
-    display(){
+    show() {
         console.log(this.empId + " " + this.empName);
     }
 }
-let Employee1 = new Employee();
-Employee1.display();
+
+class Manager extends Employee{  
+    constructor(empid: number, empname: string){
+        super(empid, empname);
+    }
+}
+
+ let man1 = new Manager(2, "kavin");
+man1.show();
+ 
+
+// let set = new Set();
+// set.add("2");
+// set.add(true);
+// set.add("kavin");
+// set.add("kavin");
+
+// let map1 = new Map();
+// map1.set("ind", "India");
+// map1.set("in", "indonesia");
+
+//map1.get();
+
+// console.log(map1.keys());
