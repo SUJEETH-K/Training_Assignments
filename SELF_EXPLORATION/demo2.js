8386a06efe5cda73db677eca7d4e93190380f982
SUJEETH-K <sujeeth4702@gmail.com>
2024-04-04 00:54:47 +0530

    changes

--- SELF_EXPLORATION/demo2.js
+++ SELF_EXPLORATION/demo2.js
@@ -18,9 +18,9 @@ console.log(name.getFullName());
 } 
 
 var wel = function () { 
-    return "hey";
+    return "Good morning";
 }
-var myname="jaikanth";
+var myname="Sujeeth";
 
 function display(myname, wel){
     console.log(wel()+' '+myname);
@@ -42,3 +42,43 @@ doSomething(() => {
     
 console.log(value);
 }
+
+function empName()
+{
+    console.log("His name is Sujeeth");
+}
+console.log(empName);//whole function is printed
+
+console.log(__filename)
+console.log(__dirname)
+
+var x = 9;
+function getName() {
+    console.log("I am Sujeeth");
+}
+getName();
+console.log(x);//I am Sujeeth 9
+
+//If we write print stmt above declaration
+getName();
+console.log(x);// I am Sujeeth undefined
+var x = 9;
+function getName() {
+    console.log("I am Sujeeth");
+}// this is called as hoisting
+
+
+var x = 3;
+empId();//   1
+console.log(x);//   3
+function empId()
+{
+    var x = 1;
+    console.log(x);
+}
+
+
+console.log(a);// not defined error
+let a = 10;
+console.log(ab);// undefined
+var ab = 20;
